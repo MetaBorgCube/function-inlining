9288466948f698cb8f6d39050ccbac5dc5d279d9
loekgugten <lvdgugten@gmail.com>
2021-11-29 20:43:19 +0100

    Initial commit

---
+++ org.metaborg.lang.tiger.statix/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  id("org.metaborg.devenv.spoofax.gradle.langspec")
+  `maven-publish`
+}
+
+val spoofax2DevenvVersion: String by ext
+spoofaxLanguageSpecification {
+  addSourceDependenciesFromMetaborgYaml.set(false)
+  addCompileDependenciesFromMetaborgYaml.set(false)
+}
+// NOTE: Should match the compile and source dependencies in metaborg.yaml,
+//  except prefixed with org.metaborg.devenv: instead of org.metaborg:
+dependencies {
+  compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.esv:$spoofax2DevenvVersion")
+  compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.template:$spoofax2DevenvVersion")
+  compileLanguage("org.metaborg.devenv:statix.lang:$spoofax2DevenvVersion")
+  compileLanguage("org.metaborg.devenv:sdf3.ext.statix:$spoofax2DevenvVersion")
+
+  sourceLanguage("org.metaborg.devenv:meta.lib.spoofax:$spoofax2DevenvVersion")
+  sourceLanguage("org.metaborg.devenv:statix.runtime:$spoofax2DevenvVersion")
+}
